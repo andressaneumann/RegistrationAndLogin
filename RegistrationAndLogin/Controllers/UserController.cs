6469e1f1aea02db4c9696aa2e77b197d83eec20c
Andressa Neumann <neumm.andressa@gmail.com>
2018-08-29 09:56:37 -0300

    Registration and login - initial setup

---
+++ RegistrationAndLogin/Controllers/UserController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RegistrationAndLogin.Models;
+
+namespace RegistrationAndLogin.Controllers
+{
+    public class UserController : Controller
+    {
+
+        //Registration Action
+
+        [HttpGet]
+        public ActionResult Registration()
+        {
+            return View();
+        }
+
+
+        //Registration POST action
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Registration([Bind(Exclude ="IsEmailVerified")] User user)
+        {
+
+            //
+
+
+            return View(user);
+        }
+
+        //Verify Email
+
+        //Verify Email LINK
+
+        //Login
+
+        //Login POST
+
+        //Logout
+
+    }
+}
